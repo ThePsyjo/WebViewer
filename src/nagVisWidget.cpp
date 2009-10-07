0f2bb156dc254965c120f8c87d9c42b6a557491e
ThePsyjo <ThePsyjo@googlemail.com>
2009-10-07 23:16:09 +0200

    header fix

--- src/nagVisWidget.cpp
+++ src/nagVisWidget.cpp
@@ -1,5 +1,5 @@
 /************************************************************************
- * webWidget.cpp                                                        *
+ * nagVisWidget.cpp                                                     *
  * Copyright (C) 2008,2009  Psyjo                                       *
  *                                                                      *
  * This program is free software; you can redistribute it and/or modify *
